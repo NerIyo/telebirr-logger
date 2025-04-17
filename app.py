dd3fb3f7378c40fdde9177e15ef04c83661c2132
Hermon tesfay <hneriyo@gmail.com>
2025-04-17 20:16:38 +0300

    Add login and logout routes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 from flask import render_template
 from flask import Flask, request, jsonify
 from models import db, PaymentLog
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "telebirr-secret-key-123"  # You can change this to anything random
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -30,6 +32,30 @@ def telebirr_webhook():
     db.session.commit()
     return jsonify({"message": "Logged successfully"}), 200
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == "admin" and password == "pass123":
+            session['user'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid credentials", 401
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    logs = PaymentLog.query.order_by(PaymentLog.timestamp.desc()).all()
+    return render_template('logs.html', logs=logs)
 
 @app.route('/logs', methods=['GET'])
 def get_logs():
